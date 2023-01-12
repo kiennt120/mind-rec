1565c21abf605dd557b2012ef4b2850e5a40c3fd
kiennt120 <kyriejad23@gmail.com>
2023-01-12 11:50:05 +0700

    first commit

---
+++ data_preprocess.py
@@ -0,0 +1,167 @@
+# from config import model_name
+import pickle
+import pandas as pd
+import numpy as np
+import csv
+import os
+from download_utils import unzip_file, maybe_download
+from nltk.tokenize import RegexpTokenizer
+from models.deeprec_utils import load_yaml, flat_config, create_hparams
+import argparse
+
+def _read_behaviors(filepath, userID):
+    with open(filepath, encoding="utf-8") as f:
+        lines = f.readlines()
+
+    user = 0
+    for line in lines:
+        splitted = line.strip('\n').split('\t')
+        if splitted[1] not in userID:
+            user += 1
+            userID[splitted[1]] = user
+    return userID
+
+def get_user(train_behaviors, utils):
+    userID = {}
+    userID = _read_behaviors(train_behaviors, userID)
+    user2index_path = os.path.join(utils, 'uid2index.pkl')
+    with open(user2index_path, 'wb') as f:
+        pickle.dump(userID, f)
+    return user2index_path
+
+def generate_word_embedding(source, target, word2int_path, word_embedding_dim):
+    """
+    Generate from pretrained word embedding file
+    If a word not in embedding file, initial its embedding by N(0, 1)
+    Args:
+        source: path of pretrained word embedding file, e.g. glove.840B.300d.txt
+        target: path for saving word embedding. Will be saved in numpy format
+        word2int_path: vocabulary file when words in it will be searched in pretrained embedding file
+        word_embedding_dim
+    """
+    # na_filter=False is needed since nan is also a valid word
+    # word, int
+    # word2int = pd.read_table(word2int_path, na_filter=False, index_col='word')
+    with open(word2int_path, 'rb') as f:
+        word2int = pickle.load(f)
+    word2int = pd.DataFrame(word2int.items(), columns=['word', 'int'])
+    word2int.set_index('word', inplace=True)
+    source = os.path.join(source, 'glove.6B.{}d.txt'.format(word_embedding_dim))
+    source_embedding = pd.read_table(source,
+                                     index_col=0,
+                                     sep=' ',
+                                     header=None,
+                                     quoting=csv.QUOTE_NONE,
+                                     names=range(word_embedding_dim))
+    # word, vector
+    source_embedding.index.rename('word', inplace=True)
+    # word, int, vector
+    merged = word2int.merge(source_embedding,
+                            how='inner',
+                            left_index=True,
+                            right_index=True)
+    merged.set_index('int', inplace=True)
+
+    missed_index = np.setdiff1d(np.arange(len(word2int) + 1),
+                                merged.index.values)
+    missed_embedding = pd.DataFrame(data=np.random.normal(
+        size=(len(missed_index), word_embedding_dim)))
+    missed_embedding['int'] = missed_index
+    missed_embedding.set_index('int', inplace=True)
+
+    final_embedding = pd.concat([merged, missed_embedding]).sort_index()
+    np.save(target, final_embedding.values)
+
+    print(
+        f'Rate of word missed in pretrained embedding: {(len(missed_index)-1)/len(word2int):.4f}'
+    )
+
+
+def download_and_extract_glove(dest_path):
+    url = "http://nlp.stanford.edu/data/glove.6B.zip"
+    filepath = maybe_download(url=url, work_directory=dest_path)
+    glove_path = os.path.join(dest_path, "glove")
+    unzip_file(filepath, glove_path, clean_zip_file=False)
+    return glove_path
+
+
+def _read_news(filepath, news_vert, news_subvert, news_words, news_entities, tokenizer):
+    vert, subvert, word, entity = 0, 0, 0, 0
+    with open(filepath, encoding="utf-8") as f:
+        lines = f.readlines()
+    for line in lines:
+        splitted = line.strip("\n").split("\t")
+        if splitted[1] not in news_vert:
+            vert += 1
+            news_vert[splitted[1]] = vert
+        if splitted[2] not in news_subvert:
+            subvert += 1
+            news_subvert[splitted[2]] = subvert
+        for i in tokenizer.tokenize(splitted[3].lower()):
+            if i not in news_words:
+                word += 1
+                news_words[i] = word
+        # news_entities[splitted[0]] = []
+        # for entity in json.loads(splitted[6]):
+        #     news_entities[splitted[0]].append(
+        #         (entity["SurfaceForms"], entity["WikidataId"])
+        #     )
+    return news_vert, news_subvert, news_words, news_entities
+
+def get_words_and_entities(train_news, utils):
+    """Load words and entities
+
+    Args:
+        train_news (str): News train file.
+        # valid_news (str): News validation file.
+        utils
+    Returns:
+        dict, dict: Words and entities dictionaries.
+    """
+    news_vert = {}
+    news_subvert = {}
+    news_words = {}
+    news_entities = {}
+    tokenizer = RegexpTokenizer(r"\w+")
+    news_vert, news_subvert, news_words, news_entities = _read_news(
+        train_news, news_vert, news_subvert, news_words, news_entities, tokenizer
+    )
+    # news_vert, news_subvert, news_words, news_entities = _read_news(
+    #     valid_news, news_vert, news_subvert, news_words, news_entities, tokenizer
+    # )
+
+    news_vert_path = os.path.join(utils, 'vert_dict.pkl')
+    with open(news_vert_path, 'wb') as f:
+        pickle.dump(news_vert, f)
+    news_subvert_path = os.path.join(utils, 'subvert_dict.pkl')
+    with open(news_subvert_path, 'wb') as f:
+        pickle.dump(news_subvert, f)
+    news_words_path = os.path.join(utils, 'word_dict.pkl')
+    with open(news_words_path, 'wb') as f:
+        pickle.dump(news_words, f)
+
+    # news_entities_path = os.path.join(utils, 'entity_dict.pkl')
+    # with open(news_entities_path, 'wb') as f:
+    #     pickle.dump(news_entities, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return news_vert_path, news_subvert_path, news_words_path
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('yaml_path', type=str)
+    parser.add_argument('data_path', type=str)
+    args = parser.parse_args()
+
+    config = load_yaml(args.yaml_path)
+    config = create_hparams(flat_config(config))
+    data_path = args.data_path
+    train_dir = os.path.join(data_path, 'train')
+    val_dir = os.path.join(data_path, 'valid')
+    test_dir = os.path.join(data_path, 'test')
+    utils = os.path.join(data_path, 'utils')
+
+    news_vert_dict_path, news_subvert_dict_path, news_word_dict_path = get_words_and_entities(os.path.join(train_dir, 'news.tsv'), utils)
+    glove_path = download_and_extract_glove(data_path)
+    generate_word_embedding(glove_path, os.path.join(utils, 'embedding.npy'), news_word_dict_path, config.word_emb_dim)
+    user2index_path = get_user(os.path.join(train_dir, 'behaviors.tsv'), utils)
---
+++ models/nrms.py
@@ -0,0 +1,283 @@
+import tensorflow.keras as keras
+from tensorflow.keras import layers
+import numpy as np
+
+
+from models.base_model import BaseModel
+from models.layers import AttLayer2, SelfAttention
+
+__all__ = ["NRMSModel"]
+
+
+class NRMSModel(BaseModel):
+    """NRMS model(Neural News Recommendation with Multi-Head Self-Attention)
+
+    Chuhan Wu, Fangzhao Wu, Suyu Ge, Tao Qi, Yongfeng Huang,and Xing Xie, "Neural News
+    Recommendation with Multi-Head Self-Attention" in Proceedings of the 2019 Conference
+    on Empirical Methods in Natural Language Processing and the 9th International Joint Conference
+    on Natural Language Processing (EMNLP-IJCNLP)
+
+    Attributes:
+        word2vec_embedding (numpy.ndarray): Pretrained word embedding matrix.
+        hparam (object): Global hyper-parameters.
+    """
+
+    def __init__(self, hparams, iterator_creator, seed=None):
+        """Initialization steps for NRMS.
+        Compared with the BaseModel, NRMS need word embedding.
+        After creating word embedding matrix, BaseModel's __init__ method will be called.
+
+        Args:
+            hparams (object): Global hyper-parameters. Some key setttings such as head_num and head_dim are there.
+            iterator_creator_train (object): NRMS data loader class for train data.
+            iterator_creator_test (object): NRMS data loader class for test and validation data
+        """
+        self.word2vec_embedding = self._init_embedding(hparams.wordEmb_file)
+
+        super().__init__(hparams, iterator_creator, seed=seed)
+
+    def _get_input_label_from_iter(self, batch_data):
+        """get input and labels for trainning from iterator
+
+        Args:
+            batch data: input batch data from iterator
+
+        Returns:
+            list: input feature fed into model (clicked_title_batch & candidate_title_batch)
+            numpy.ndarray: labels
+        """
+        input_feat = [
+            batch_data["clicked_title_batch"],
+            batch_data["clicked_ab_batch"],
+            batch_data["clicked_vert_batch"],
+            batch_data["clicked_subvert_batch"],
+            batch_data["candidate_title_batch"],
+            batch_data["candidate_ab_batch"],
+            batch_data["candidate_vert_batch"],
+            batch_data["candidate_subvert_batch"],
+        ]
+        input_label = batch_data["labels"]
+        return input_feat, input_label
+
+    def _get_user_feature_from_iter(self, batch_data):
+        """get input of user encoder
+        Args:
+            batch_data: input batch data from user iterator
+
+        Returns:
+            numpy.ndarray: input user feature (clicked title batch)
+        """
+        input_feature = [
+            batch_data["clicked_title_batch"],
+            batch_data["clicked_ab_batch"],
+            batch_data["clicked_vert_batch"],
+            batch_data["clicked_subvert_batch"],
+        ]
+        input_feature = np.concatenate(input_feature, axis=-1)
+        return input_feature
+
+    def _get_news_feature_from_iter(self, batch_data):
+        """get input of news encoder
+        Args:
+            batch_data: input batch data from news iterator
+
+        Returns:
+            numpy.ndarray: input news feature (candidate title batch)
+        """
+        input_feature = [
+            batch_data["candidate_title_batch"],
+            batch_data["candidate_ab_batch"],
+            batch_data["candidate_vert_batch"],
+            batch_data["candidate_subvert_batch"],
+        ]
+        input_feature = np.concatenate(input_feature, axis=-1)
+        return input_feature
+
+    def _build_graph(self):
+        """Build NRMS model and scorer.
+
+        Returns:
+            object: a model used to train.
+            object: a model used to evaluate and inference.
+        """
+        model, scorer = self._build_nrms()
+        return model, scorer
+
+    def _build_userencoder(self, newsencoder):
+        """The main function to create user encoder of NRMS.
+
+        Args:
+            newsencoder (object): the news encoder of NRMS.
+
+        Return:
+            object: the user encoder of NRMS.
+        """
+        hparams = self.hparams
+        his_input_news = keras.Input(
+            shape=(hparams.his_size, hparams.title_size+hparams.body_size+2), dtype="int32"
+        )
+
+        click_news_presents = layers.TimeDistributed(newsencoder)(his_input_news)
+        y = SelfAttention(hparams.head_num, hparams.head_dim, seed=self.seed)(
+            [click_news_presents] * 3
+        )
+        user_present = AttLayer2(hparams.attention_hidden_dim, seed=self.seed)(y)
+
+        model = keras.Model(his_input_news, user_present, name="user_encoder")
+        return model
+
+    def _build_newsencoder(self, embedding_layer):
+        """The main function to create news encoder of NRMS.
+
+        Args:
+            embedding_layer (object): a word embedding layer.
+
+        Return:
+            object: the news encoder of NRMS.
+        """
+        hparams = self.hparams
+        input_title_body_verts = keras.Input(
+            shape=(hparams.title_size + hparams.body_size + 2,), dtype="int32"
+        )
+
+        sequences_input_title = layers.Lambda(lambda x: x[:, : hparams.title_size])(input_title_body_verts)
+        sequences_input_body = layers.Lambda(lambda x: x[:, hparams.title_size: hparams.title_size + hparams.body_size])(input_title_body_verts)
+        input_vert = layers.Lambda(lambda x: x[:, hparams.title_size + hparams.body_size: hparams.title_size + hparams.body_size + 1])(input_title_body_verts)
+        input_subvert = layers.Lambda(lambda x: x[:, hparams.title_size + hparams.body_size + 1:])(input_title_body_verts)
+
+        title_repr = self._build_titleencoder(embedding_layer)(sequences_input_title)
+        body_repr = self._build_bodyencoder(embedding_layer)(sequences_input_body)
+        vert_repr = self._build_vertencoder()(input_vert)
+        subvert_repr = self._build_subvertencoder()(input_subvert)
+        concate_repr = layers.Concatenate(axis=-2)([title_repr, body_repr, vert_repr, subvert_repr])
+        news_repr = AttLayer2(hparams.attention_hidden_dim, seed=self.seed)(concate_repr)
+
+        model = keras.Model(input_title_body_verts, news_repr, name='news_encoder')
+        return model
+
+    def _build_titleencoder(self, embedding_layer):
+        hparams = self.hparams
+        sequences_input_title = keras.Input(shape=(hparams.title_size,), dtype='int32')
+        embedded_sequences_title = embedding_layer(sequences_input_title)
+
+        y = layers.Dropout(hparams.dropout)(embedded_sequences_title)
+        y = SelfAttention(hparams.head_num, hparams.head_dim, seed=self.seed)([y, y, y])
+        y = layers.Dropout(hparams.dropout)(y)
+
+        pred_title = AttLayer2(hparams.attention_hidden_dim, seed=self.seed)(y)
+        pred_title = layers.Reshape((1, hparams.filter_num))(pred_title)
+        model = keras.Model(sequences_input_title, pred_title, name='title_encoder')
+        return model
+
+    def _build_bodyencoder(self, embedding_layer):
+        hparams = self.hparams
+        sequences_input_body = keras.Input(shape=(hparams.body_size,), dtype='int32')
+        embedded_sequences_body = embedding_layer(sequences_input_body)
+
+        y = layers.Dropout(hparams.dropout)(embedded_sequences_body)
+        y = SelfAttention(hparams.head_num, hparams.head_dim, seed=self.seed)([y, y, y])
+        y = layers.Dropout(hparams.dropout)(y)
+
+        pred_body = AttLayer2(hparams.attention_hidden_dim, seed=self.seed)(y)
+        pred_body = layers.Reshape((1, hparams.filter_num))(pred_body)
+
+        model = keras.Model(sequences_input_body, pred_body, name='body_encoder')
+        return model
+
+    def _build_vertencoder(self):
+        hparams = self.hparams
+        input_vert = keras.Input(shape=(1,), dtype="int32")
+
+        vert_embedding = layers.Embedding(
+            hparams.vert_num, hparams.vert_emb_dim, trainable=True
+        )
+
+        vert_emb = vert_embedding(input_vert)
+        pred_vert = layers.Dense(
+            hparams.filter_num,
+            activation=hparams.dense_activation,
+            bias_initializer=keras.initializers.Zeros(),
+            kernel_initializer=keras.initializers.glorot_uniform(seed=self.seed),
+        )(vert_emb)
+        pred_vert = layers.Reshape((1, hparams.filter_num))(pred_vert)
+
+        model = keras.Model(input_vert, pred_vert, name="vert_encoder")
+        return model
+
+    def _build_subvertencoder(self):
+        hparams = self.hparams
+        input_subvert = keras.Input(shape=(1,), dtype='int32')
+        subvert_embedding = layers.Embedding(hparams.subvert_num, hparams.subvert_emd_dim, trainable=True)
+
+        subvert_emd = subvert_embedding(input_subvert)
+        pred_subvert = layers.Dense(
+            hparams.filter_num,
+            activation=hparams.dense_activation,
+            bias_initializer=keras.initializers.Zeros(),
+            kernel_initializer=keras.initializers.glorot_uniform(seed=self.seed),
+        )(subvert_emd)
+        pred_subvert = layers.Reshape((1, hparams.filter_num))(pred_subvert)
+
+        model = keras.Model(input_subvert, pred_subvert, name='subvert_encoder')
+        return model
+
+    def _build_nrms(self):
+        """The main function to create NRMS's logic. The core of NRMS
+        is a user encoder and a news encoder.
+
+        Returns:
+            object: a model used to train.
+            object: a model used to evaluate and inference.
+        """
+        hparams = self.hparams
+
+        his_input_title = keras.Input(
+            shape=(hparams.his_size, hparams.title_size), dtype="int32"
+        )
+        his_input_body = keras.Input(shape=(hparams.his_size, hparams.body_size), dtype='int32')
+        his_input_vert = keras.Input(shape=(hparams.his_size, 1), dtype='int32')
+        his_input_subvert = keras.Input(shape=(hparams.his_size, 1), dtype='int32')
+
+        pred_input_title = keras.Input(
+            shape=(hparams.npratio + 1, hparams.title_size), dtype="int32"
+        )
+        pred_input_body = keras.Input(shape=(hparams.npratio+1, hparams.body_size), dtype='int32')
+        pred_input_vert = keras.Input(shape=(hparams.npratio+1, 1), dtype='int32')
+        pred_input_subvert = keras.Input(shape=(hparams.npratio + 1, 1), dtype='int32')
+
+        pred_input_title_one = keras.Input(shape=(1, hparams.title_size), dtype="int32")
+        pred_input_body_one = keras.Input(shape=(1, hparams.body_size), dtype='int32')
+        pred_input_vert_one = keras.Input(shape=(1, 1), dtype='int32')
+        pred_input_subvert_one = keras.Input(shape=(1, 1), dtype='int32')
+
+        his_title_body_verts = layers.Concatenate(axis=-1)([his_input_title, his_input_body, his_input_vert, his_input_subvert])
+        pred_title_body_verts = layers.Concatenate(axis=-1)([pred_input_title, pred_input_body, pred_input_vert, pred_input_subvert])
+
+        pred_title_body_verts_one = layers.Concatenate(axis=-1)([pred_input_title_one, pred_input_body_one, pred_input_vert_one, pred_input_subvert_one])
+        pred_title_body_verts_one = layers.Reshape((-1,))(pred_title_body_verts_one)
+
+        embedding_layer = layers.Embedding(
+            self.word2vec_embedding.shape[0],
+            hparams.word_emb_dim,
+            weights=[self.word2vec_embedding],
+            trainable=True,
+        )
+
+        newsencoder = self._build_newsencoder(embedding_layer)
+        self.userencoder = self._build_userencoder(newsencoder)
+        self.newsencoder = newsencoder
+
+        user_present = self.userencoder(his_title_body_verts)
+        news_present = layers.TimeDistributed(self.newsencoder)(pred_title_body_verts)
+        news_present_one = self.newsencoder(pred_title_body_verts_one)
+
+        preds = layers.Dot(axes=-1)([news_present, user_present])
+        preds = layers.Activation(activation="softmax")(preds)
+
+        pred_one = layers.Dot(axes=-1)([news_present_one, user_present])
+        pred_one = layers.Activation(activation="sigmoid")(pred_one)
+
+        model = keras.Model([his_input_title, his_input_body, his_input_vert, his_input_subvert, pred_input_title, pred_input_body, pred_input_vert, pred_input_subvert], preds)
+        scorer = keras.Model([his_input_title, his_input_body, his_input_vert, his_input_subvert, pred_input_title_one, pred_input_body_one, pred_input_vert_one, pred_input_subvert_one], pred_one)
+
+        return model, scorer
