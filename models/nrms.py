063d6ac9b44ed32185fb2f85d32fffcfa01da4ae
kiennt120 <kyriejad23@gmail.com>
2023-01-18 09:28:30 +0700

    update entity

--- models/nrms.py
+++ models/nrms.py
@@ -157,10 +157,15 @@ class NRMSModel(BaseModel):
 
         title_repr = self._build_titleencoder(embedding_layer)(sequences_input_title)
         entity_title_repr = self._build_entity_title(entity_embedding_layer)(sequences_entity_input_title)
-        title_repr = layers.Add()([title_repr, entity_title_repr])
+        title_repr = layers.Concatenate(axis=-2)([title_repr, entity_title_repr])
+        title_repr = layers.Dense(hparams.attention_hidden_dim)(title_repr)
+        # title_repr = layers.Add()([title_repr, entity_title_repr])
         body_repr = self._build_bodyencoder(embedding_layer)(sequences_input_body)
         entity_ab_repr = self._build_entity_ab(entity_embedding_layer)(sequences_entity_input_ab)
-        body_repr = layers.Add()([body_repr, entity_ab_repr])
+        print(entity_ab_repr.get_shape())
+        body_repr = layers.Concatenate(axis=-2)([body_repr, entity_ab_repr])
+        body_repr = layers.Dense(hparams.attention_hidden_dim)(body_repr)
+        # body_repr = layers.Add()([body_repr, entity_ab_repr])
         vert_repr = self._build_vertencoder()(input_vert)
         subvert_repr = self._build_subvertencoder()(input_subvert)
         concate_repr = layers.Concatenate(axis=-2)([title_repr, body_repr, vert_repr, subvert_repr])
@@ -182,9 +187,6 @@ class NRMSModel(BaseModel):
         pred_title = layers.Reshape((1, hparams.filter_num))(pred_title)
         model = keras.Model(sequences_input_title, pred_title, name='title_encoder')
         return model
-
-    # def _build_add_title_entity(self, embedding_layer, entity_embedding_layer):
-    #     hparams = self.hparams
 
     def _build_entity_title(self, entity_embedding_layer):
         hparams = self.hparams
